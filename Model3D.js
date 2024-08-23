9e9dc8554977d04d0979eadc7c1f5432a87bd82b
telcoZidane <zidane.telcosolution@gmail.com>
2024-08-23 12:26:30 +0100

    fixing moving camera and opacity

--- Model3D.js
+++ Model3D.js
@@ -8,6 +8,7 @@ class Model3D {
         this.position = position;
         this.scale = scale;
         this.loader = new GLTFLoader();
+        this.clicked = false;
     }
 
     loadModel() {
@@ -18,10 +19,13 @@ class Model3D {
                 this.model.position.set(this.position.x, this.position.y, this.position.z);
                 this.model.scale.set(this.scale, this.scale, this.scale);
                 this.scene.add(this.model);
-                console.log("zidane" +""+  this.position.x + ""+ this.position.y+""+ this.position.z );
 
-                console.log('Model loaded:', this.url);
-                console.log(this.model);
+                this.model.traverse((child) => {
+                    if (child.isMesh) {
+                        child.userData = this;
+                        child.on('click', (event) => this.onClick(event));
+                    }
+                });
             },
             undefined,
             (error) => {
@@ -29,41 +33,11 @@ class Model3D {
             }
         );
     }
+
+    onClick(event) {
+        console.log("Clicked model at:", this.position);
+        this.clicked = true;
+    }
 }
 
 export default Model3D;
-
-/////////////////////////////////////////////////////////////////
-// import * as THREE from 'three';
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-// class Model3D {
-//     constructor(scene, url, position = { x: 0, y: 0, z: 0 }, scale = 1, name = '') {
-//         this.scene = scene;
-//         this.url = url;
-//         this.position = position;
-//         this.scale = scale;
-//         this.name = name;
-//         this.loader = new GLTFLoader();
-//     }
-
-//     loadModel() {
-//         this.loader.load(
-//             this.url,
-//             (gltf) => {
-//                 this.model = gltf.scene;
-//                 this.model.position.set(this.position.x, this.position.y, this.position.z);
-//                 this.model.scale.set(this.scale, this.scale, this.scale);
-//                 this.model.name = this.name; 
-//                 this.scene.add(this.model);
-//                 console.log('Model loaded:', this.url);
-//             },
-//             undefined,
-//             (error) => {
-//                 console.error('Error loading model:', error);
-//             }
-//         );
-//     }
-// }
-
-// export default Model3D;
--- main.js
+++ main.js
@@ -27,14 +27,20 @@ scene.add(leftlight);
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
 
-const officeModel = new Model3D(scene, '/model/Office_base3.glb', { x: 0, y: 0, z: 0 }, 2).loadModel();
-const pcModel = new Model3D(scene, '/model/ModelPc.glb', { x: -5, y: -3.1, z: 1 }, 1).loadModel();
-const bicycleModel = new Model3D(scene, '/model/Bicycle.glb', { x: -5, y: -3.6, z: -1 }, 1).loadModel();
+const officeModel = new Model3D(scene, '/model/Office_base3.glb', { x: 0, y: 0, z: 0 }, 2);
+const pcModel = new Model3D(scene, '/model/ModelPc.glb', { x: -5, y: -3.1, z: 1 }, 1);
+const bicycleModel = new Model3D(scene, '/model/Bicycle.glb', { x: -5, y: -3.6, z: -1 }, 1);
 
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
+officeModel.loadModel();
+pcModel.loadModel();
+bicycleModel.loadModel();
+
+const models = [officeModel, pcModel, bicycleModel];
 
 function onMouseClick(event) {
+    const mouse = new THREE.Vector2();
+    const raycaster = new THREE.Raycaster();
+
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -43,65 +49,48 @@ function onMouseClick(event) {
 
     if (intersects.length > 0) {
         const target = intersects[0].object;
-        const targetNames = ['ModelPc', 'Bicycle']; // List of clickable object names
+        const model = target.userData;
 
-        // if (targetNames.includes(target.name)) {
-            const targetPosition = new THREE.Vector3();
-            target.getWorldPosition(targetPosition);
-
-            const duration = 1000; 
-            const startPosition = camera.position.clone();
-            const endPosition = targetPosition.clone().add(new THREE.Vector3(-2, 1, 0));
-            const startTime = performance.now();
-
-            function animateCamera(time) {
-                const elapsed = time - startTime;
-                const t = Math.min(elapsed / duration, 1);
-                camera.position.lerpVectors(startPosition, endPosition, t);
-                if (t < 1) {
-                    requestAnimationFrame(animateCamera);
-                } else {
-                    camera.lookAt(targetPosition);
-                }
-            }
-            requestAnimationFrame(animateCamera);
-
-            // Make other objects semi-transparent
-            scene.children.forEach((child) => {
-                if (child !== target.parent) {
+        // Reset opacity for all models
+        models.forEach((model) => {
+            if (model.model) {
+                model.clicked = false;
+                model.model.traverse((child) => {
                     if (child.material) {
                         child.material.transparent = true;
                         child.material.opacity = 0.5;
-                    } else {
-                        child.traverse((node) => {
-                            if (node.material) {
-                                node.material.transparent = true;
-                                node.material.opacity = 0.5;
-                            }
-                        });
                     }
-                }
-            });
+                });
+            }
+        });
 
-            // Restore transparency on next click outside the object
-            window.addEventListener('click', (event) => {
-                if (!raycaster.intersectObjects(scene.children, true).length) {
-                    scene.children.forEach((child) => {
-                        if (child.material) {
-                            child.material.transparent = false;
-                            child.material.opacity = 1;
-                        } else {
-                            child.traverse((node) => {
-                                if (node.material) {
-                                    node.material.transparent = false;
-                                    node.material.opacity = 1;
-                                }
-                            });
-                        }
-                    });
-                }
-            }, { once: true });
-        
+        // Set clicked model opacity to 1
+        model.clicked = true;
+        model.model.traverse((child) => {
+            if (child.material) {
+                child.material.transparent = false;
+                child.material.opacity = 1;
+            }
+        });
+
+        const targetPosition = new THREE.Vector3();
+        target.getWorldPosition(targetPosition);
+
+        const duration = 1000;
+        const startPosition = camera.position.clone();
+        const endPosition = targetPosition.clone().add(new THREE.Vector3(-3, 0.5, 1));
+        const startTime = performance.now();
+
+        function animateCamera(time) {
+            const elapsed = time - startTime;
+            const t = Math.min(elapsed / duration, 1);
+            camera.position.lerpVectors(startPosition, endPosition, t);
+            camera.lookAt(targetPosition);
+            if (t < 1) {
+                requestAnimationFrame(animateCamera);
+            }
+        }
+        requestAnimationFrame(animateCamera);
     }
 }
 
@@ -114,3 +103,4 @@ function animate() {
 }
 
 animate();
+
